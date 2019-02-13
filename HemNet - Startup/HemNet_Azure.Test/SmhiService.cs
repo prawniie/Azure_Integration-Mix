d6d114b8adbc42450259e6c8530bb193f92282dc
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-02-13 11:01:15 +0100

    Kan plocka ut värdet för temp för en viss tidsserie

--- HemNet - Startup/HemNet_Azure.Test/SmhiService.cs
+++ HemNet - Startup/HemNet_Azure.Test/SmhiService.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using static HemNet_Azure.Test.SmhiClasses;
 
 namespace HemNet_Azure.Test
 {
@@ -20,6 +23,19 @@ namespace HemNet_Azure.Test
 
                 return await content.ReadAsStringAsync();
             }
+        }
+        public async Task<Rootobject> GetMeteorologicalForecast(decimal longitude, decimal latitude)
+        {
+            string sLongitude = Math.Round(longitude, 3).ToString(new CultureInfo("en"));
+            string sLatitude = Math.Round(latitude, 3).ToString(new CultureInfo("en"));
+
+            string page = $"https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/{sLongitude}/lat/{sLatitude}/data.json";
+
+            var smhi = new SmhiService();
+
+            var result = await smhi.Get(page);
+            return JsonConvert.DeserializeObject<Rootobject>(result);
+
         }
     }
 }
--- HemNet - Startup/HemNet_Azure.Test/UnitTest1.cs
+++ HemNet - Startup/HemNet_Azure.Test/UnitTest1.cs
@@ -1,6 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
+using static HemNet_Azure.Test.SmhiClasses;
 
 namespace HemNet_Azure.Test
 {
@@ -20,10 +24,25 @@ namespace HemNet_Azure.Test
         }
 
         [TestMethod]
-        public void GetForecastStringViaSmhiServiceAsync()
+        public void GetForecastAsStringSmhiServiceAsync()
         {
             var smhi = new SmhiService();
             string forecast = smhi.Get("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.07/lat/59.33/data.json").Result;
+        }
+
+        [TestMethod]
+        public void ExtractInfoFromForecast()
+        {
+            var smhi = new SmhiService();
+
+            decimal longitud = 11.9751m;
+            decimal latitud = 57.7087m;
+
+            Rootobject rootObject = smhi.GetMeteorologicalForecast(longitud, latitud).Result;
+            //var result = rootObject.timeSeries[0].parameters.Where(p => p.name == "t");
+            decimal result = rootObject.timeSeries[0].parameters[11].values[0];
+
+
         }
     }
 }
